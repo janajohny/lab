a0317682a783f711edb70087c40a3e07dfbe8aa0
janajohny <56085771+janajohny@users.noreply.github.com>
2021-11-17 19:20:14 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo -e "[ansiblevm]\n$(az vm show -d -g linuxVMRg -n ansiblevm --query publicIps -o tsv)" > /home/vmadmin/hosts
+echo -e "[linuxVM1]\n$(az vm show -d -g linuxVMRg -n linuxVM1 --query publicIps -o tsv)" > /home/vmadmin/hosts
 cat /home/vmadmin/hosts
